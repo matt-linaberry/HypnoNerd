d26cc51cd98fe5d9e50d48572bd4e44832067450
Matt Linaberry <matt.linaberry@we-ics.com>
2014-09-20 13:26:36 -0400

    before adding the next view controller

---
+++ HypnoNerd/BNRReminderViewController.h
@@ -0,0 +1,14 @@
+//
+//  BNRReminderViewController.h
+//  HypnoNerd
+//
+//  Created by Matthew Linaberry on 9/20/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRReminderViewController : UIViewController
+
+
+@end
---
+++ HypnoNerd/BNRReminderViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.m
+//  HypnoNerd
+//
+//  Created by Matthew Linaberry on 9/20/14.
+//
+//
+
+#import "BNRReminderViewController.h"
+
+@implementation BNRReminderViewController
+
+@end
